01dfd30672833d0c6b0aaea30419b3da6688446f
Ciscol <13600204885@163.com>
2020-03-03 03:53:16 +0800

    'test-branch'

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from random import *
 from flask_cors import CORS
 
+# flaks app init
 app = Flask(__name__,
             static_folder="../dist/static",  # 设置静态文件夹目录
             template_folder="../dist")  # 设置vue编译输出目录dist文件夹，为Flask模板文件目录
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
+# routes
 @app.route('/api/random')
 def random_number():
     response = {
@@ -16,6 +18,18 @@ def random_number():
     return jsonify(response)
 
 
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    response = {
+        'state': 1,
+        'token': 'TT123456789',
+        'message': 'user error'
+    }
+    return response
+
+
+# url redirect 所有的url将被重定向至index.html，由前端vue-router进行分发
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
